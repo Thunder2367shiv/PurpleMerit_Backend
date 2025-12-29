c801c3e51aac7b5050aee02e74068e4b4e3c8d65
Thunder2367shiv <2022kuec2034@iiitkota.ac.in>
2025-12-30 00:57:50 +0530

    cors update

--- server.js
+++ server.js
@@ -10,9 +10,10 @@ connectDB();
 const app = express();
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: true, // This allows any origin to connect even with credentials
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json()); // Body parser [cite: 56]
 
